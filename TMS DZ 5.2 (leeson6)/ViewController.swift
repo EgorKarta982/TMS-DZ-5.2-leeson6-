816f0229955d80c5c049bb1405a14c8b05646c56
Egor Kartshev <egorkarta982@gmail.com>
2022-06-13 16:22:06 +0300

    Initial Commit

---
+++ TMS DZ 5.2 (leeson6)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMS DZ 5.2 (leeson6)
+//
+//  Created by Егор on 13.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
